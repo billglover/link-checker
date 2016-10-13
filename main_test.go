c0f58464d0d4c8f4c234f2d2f24eb620d9a5f038
Bill Glover <bill@billglover.co.uk>
2016-10-13 21:07:18 +0100

    Convert tests to table tests

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 const checkMark = "\u2713"
 const ballotX = "\u2717"
@@ -8,25 +11,40 @@ const ballotX = "\u2717"
 // TestUrlChecker validates that the urlChecker function can fetch a url
 // and return the HTTP status code
 func TestUrlChecker(t *testing.T) {
-	url := "http://www.billglover.me/"
-	statusCode := 200
+
+	var urls = []struct {
+		url        string
+		statusCode int
+	}{
+		{
+			"http://www.billglover.me/",
+			http.StatusOK,
+		},
+		{
+			"http://www.billglover.me/null",
+			http.StatusNotFound,
+		},
+	}
 
 	t.Log("Given the need to test checking a URL.")
 	{
-		t.Logf("\tWhen checking \"%s\" for status code \"%d\"", url, statusCode)
-		{
-			code, err := getStatusCode(url)
-			if err != nil {
-				t.Fatal("\t\tShould be able to make the Get call.", ballotX, err)
+		for _, u := range urls {
+			t.Logf("\tWhen checking \"%s\" for status code \"%d\"", u.url, u.statusCode)
+			{
+				code, err := getStatusCode(u.url)
+				if err != nil {
+					t.Fatal("\t\tShould be able to make the Get call.", ballotX, err)
+				}
+
+				t.Log("\t\tShould be able to make the Get call.", checkMark)
+
+				if code == u.statusCode {
+					t.Logf("\t\tShould receive a \"%d\" status. %v", u.statusCode, checkMark)
+				} else {
+					t.Errorf("\t\tShould receive a \"%d\" status. %v %v", u.statusCode, ballotX, code)
+				}
 			}
 
-			t.Log("\t\tShould be able to make the Get call.", checkMark)
-
-			if code == statusCode {
-				t.Logf("\t\tShould receive a \"%d\" status. %v", statusCode, checkMark)
-			} else {
-				t.Errorf("\t\tShould receive a \"%d\" status. %v %v", statusCode, ballotX, code)
-			}
 		}
 	}
 }
